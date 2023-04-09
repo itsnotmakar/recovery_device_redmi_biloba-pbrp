008db72eb0758de413792232a2e84fb9a5672463
True Eye <98771969+npcx42@users.noreply.github.com>
2023-04-09 16:39:24 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,6 +144,11 @@ ifneq ($(OF_HIDE_NOTCH),1)
     TW_STATUS_ICONS_ALIGN := center
 endif
 
+# exFAT FS Support
+TW_INCLUDE_FUSE_EXFAT := true
+# NTFS Support
+TW_INCLUDE_FUSE_NTFS := true
+
 # Decryption
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_FBE_METADATA_DECRYPT := true
